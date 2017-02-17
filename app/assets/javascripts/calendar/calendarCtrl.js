30aae675dc95cdabea79b29879aa64593d457115
James Maynard <jameskirkmaynard@gmail.com>
2017-02-17 08:23:37 -0800

    Update calendarCtrl to have correct start times

--- app/assets/javascripts/calendar/calendarCtrl.js
+++ app/assets/javascripts/calendar/calendarCtrl.js
@@ -4,7 +4,21 @@ angular.module('motherFoucaults')
   '$compile',
   'events',
   function($scope, $compile, events) {
-    $scope.events = events.events;
+    $scope.events = [];
+    events.events.forEach(function(index) {
+      var d, m, y;
+      newDate = new Date(index.date);
+      d = newDate.getDate();
+      m = newDate.getMonth();
+      y = newDate.getFullYear();
+      hr = index.time === null ? 19 : index.time;
+      $scope.events.push({
+        title: index.title,
+        date: index.date,
+        time: index.time === null ? hr : index.time,
+        start: new Date(y, m, d, hr)
+      });
+    });
     $scope.eventRender = function (events, element, view) {
       element.attr({
         'tooltip': event.title,
